6ee0403df7f5a7c65aeeaefbb87503bb80a0e976
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-01-19 21:57:34 +0700

    Pickup Respawning Themselves Automatically

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace RPG.Combat
@@ -6,6 +7,7 @@ namespace RPG.Combat
     {
         #region --Fields-- (Inspector)
         [SerializeField] private Weapon _pickupWeapon;
+        [SerializeField] private float _respawnTime = 5f;
         #endregion
 
 
@@ -19,9 +21,37 @@ namespace RPG.Combat
                 if (fighter == null) return;
 
                 fighter.EquippedWeapon(_pickupWeapon);
-
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(_respawnTime));
             }
+        }
+        #endregion
+
+
+
+        #region --Methods-- (Custom PRIVATE)
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            HidePickup();
+
+            yield return new WaitForSeconds(seconds);
+
+            ShowPickup();
+        }
+
+        private void HidePickup()
+        {
+            GetComponent<CapsuleCollider>().enabled = false;
+
+            foreach (Transform eachChild in transform)
+                eachChild.gameObject.SetActive(false);
+        }
+
+        private void ShowPickup()
+        {
+            GetComponent<CapsuleCollider>().enabled = true;
+
+            foreach (Transform eachChild in transform)
+                eachChild.gameObject.SetActive(true);
         }
         #endregion
     }

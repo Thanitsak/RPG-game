93642a458ade80bac45e2645e29075d50aead187
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-01-20 13:31:20 +0700

    Progression Design Setup

---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    public class BaseStats : MonoBehaviour
+    {
+        #region --Fields-- (Inspector)
+        [Range(1, 99)]
+        [SerializeField] private int _startingLevel = 1;
+        [SerializeField] private CharacterClass _characterClass;
+        #endregion
+    }
+}

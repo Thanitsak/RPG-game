ee7cf8d42cffde88f5f3ce805653b01da56c1266
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-02-06 17:03:17 +0700

    Weapon Hit SFX with Louder Audio Listener

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
+        #region --Events-- (UnityEvent)
+        [Header("UnityEvent")]
+        [SerializeField] private UnityEvent _onHit;
+        #endregion
+
+
         #region --Methods-- (Built In)
         public void OnHit()
         {
-            print("Weapon hit " + gameObject.name);
+            _onHit?.Invoke();
         }
         #endregion
     }

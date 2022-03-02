d834096ccfb74940fa278542892d4809df4e2088
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-03-02 20:37:52 +0700

    Styling Nodes

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEditor.Callbacks;
+using UnityEngine;
 
 namespace RPG.Dialogue.Editor
 {
@@ -7,49 +8,12 @@ namespace RPG.Dialogue.Editor
     {
         #region --Fields-- (In Class)
         private Dialogue _selectedDialogue = null;
+        private GUIStyle _nodeStyle = null;
         #endregion
 
 
 
-        #region --Methods-- (Built In)
-        private void OnGUI()
-        {
-            if (_selectedDialogue == null)
-            {
-                EditorGUILayout.LabelField("No Dialogue Selected");
-            }
-            else
-            {
-                foreach (DialogueNode eachNode in _selectedDialogue.Nodes)
-                {
-                    EditorGUI.BeginChangeCheck();
-
-                    EditorGUILayout.LabelField("Node : ");
-                    string newID = EditorGUILayout.TextField($"{eachNode.uniqueID}");
-                    string newText = EditorGUILayout.TextField($"{eachNode.text}");
-
-                    if (EditorGUI.EndChangeCheck())
-                    {
-                        Undo.RecordObject(_selectedDialogue, "Update Dialogue Field"); // This will undo only one at a time because once changes is made on any of the field this will trigger right away
-
-                        eachNode.text = newText;
-                        eachNode.uniqueID = newID;
-
-                        EditorUtility.SetDirty(_selectedDialogue);
-                    }
-                }
-            }
-        }
-
-        private void OnSelectionChange()
-        {
-            RefreshDialogueWindow();
-        }
-        #endregion
-
-
-
-        #region --Methods-- (Custom PRIVATE) ~Annotation Callback~
+        #region --Methods-- (Annotation)
         [MenuItem("Window/RPG/Dialogue Window", false, 10000)]
         private static void ShowEditorWindow()
         {
@@ -65,14 +29,70 @@ namespace RPG.Dialogue.Editor
                 ShowEditorWindow();
                 return true;
             }
-            
+
             return false;
         }
         #endregion
 
 
 
+        #region --Methods-- (Built In)
+        private void OnEnable()
+        {
+            _nodeStyle = new GUIStyle();
+            _nodeStyle.normal.background = EditorGUIUtility.Load("node0") as Texture2D;
+            _nodeStyle.padding = new RectOffset(15, 15, 15, 15);
+            _nodeStyle.border = new RectOffset(12, 12, 12, 12);
+            //_nodeStyle.normal.textColor = Color.white; // Not get any effect
+        }
+
+        private void OnGUI()
+        {
+            if (_selectedDialogue == null)
+            {
+                EditorGUILayout.LabelField("No Dialogue Selected");
+            }
+            else
+            {
+                foreach (DialogueNode eachNode in _selectedDialogue.Nodes)
+                {
+                    OnGUINode(eachNode);
+                }
+            }
+        }
+
+        private void OnSelectionChange()
+        {
+            RefreshDialogueWindow();
+        }
+        #endregion
+
+
+
         #region --Methods-- (Custom PRIVATE)
+        private void OnGUINode(DialogueNode node)
+        {
+            GUILayout.BeginArea(node.position, _nodeStyle);
+
+            EditorGUI.BeginChangeCheck();
+
+            EditorGUILayout.LabelField("Node : ", EditorStyles.whiteLargeLabel);
+            string newID = EditorGUILayout.TextField($"{node.uniqueID}");
+            string newText = EditorGUILayout.TextField($"{node.text}");
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(_selectedDialogue, "Update Dialogue Field"); // This will undo only one at a time because once changes is made on any of the field this will trigger right away
+
+                node.text = newText;
+                node.uniqueID = newID;
+
+                EditorUtility.SetDirty(_selectedDialogue);
+            }
+
+            GUILayout.EndArea();
+        }
+
         private void RefreshDialogueWindow()
         {
             if (Selection.activeObject is Dialogue newDialogue)

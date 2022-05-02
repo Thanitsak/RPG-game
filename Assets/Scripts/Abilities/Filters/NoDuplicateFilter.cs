6b506f61f6b71bd28619884fff08b2e3497107df
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-05-02 21:24:28 +0700

    Add New NoDuplicate Filter

---
+++ Assets/Scripts/Abilities/Filters/NoDuplicateFilter.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Abilities.Filters
+{
+    [CreateAssetMenu(fileName = "Untitled NoDuplicate Filter", menuName = "RPG/Game Item/Filters/New NoDuplicate Filter", order = 128)]
+    public class NoDuplicateFilter : FilterStrategy
+    {
+        #region --Fields-- (In Class)
+        private List<GameObject> _gameObjectsDatabase = new List<GameObject>();
+        #endregion
+
+
+
+        #region --Methods-- (Override)
+        public override IEnumerable<GameObject> Filter(IEnumerable<GameObject> objectsToFilter)
+        {
+            _gameObjectsDatabase.Clear(); // have to clear out each time otherwise second time no object will be returned
+
+            foreach (GameObject eachObject in objectsToFilter)
+            {
+                if (!_gameObjectsDatabase.Contains(eachObject))
+                {
+                    _gameObjectsDatabase.Add(eachObject);
+                    yield return eachObject;
+                }
+            }
+        }
+        #endregion
+    }
+}
